83c5c1d4f4645fe7b24aac686308b8dec3b3fee3
Devansh Bajaj <devansh21db.db@gmail.com>
2020-07-02 21:57:03 +0530

    Update settings.py

--- djangoapi/settings.py
+++ djangoapi/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oog0xeo)vzh8+jmr_7od+h^#ho761s#(!)-2#7)2l&^l*vcwd0'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
